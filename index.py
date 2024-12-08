eae67f58a427bc8eb4c526b6fbc8560636fab754
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-12-08 08:59:04 -0800

    updates

--- index.py
+++ index.py
@@ -682,9 +682,9 @@ def iterate_assets(interval_seconds, data_points_for_x_minutes):
                         elif current_price < lower_band:
                             print('~ BUY OPPORTUNITY (price below lower Bollinger Band)~')
                             place_market_buy_order(symbol, SHARES_TO_ACQUIRE)
-                        elif expected_profit_percentage >= TARGET_PROFIT_PERCENTAGE:
-                            print('~ BUY OPPORTUNITY (expected_profit_percentage >= TARGET_PROFIT_PERCENTAGE)~')
-                            place_market_buy_order(symbol, SHARES_TO_ACQUIRE)
+                        # elif expected_profit_percentage >= TARGET_PROFIT_PERCENTAGE:
+                        #     print('~ BUY OPPORTUNITY (expected_profit_percentage >= TARGET_PROFIT_PERCENTAGE)~')
+                        #     place_market_buy_order(symbol, SHARES_TO_ACQUIRE)
 
                 elif looking_to_sell:
 
