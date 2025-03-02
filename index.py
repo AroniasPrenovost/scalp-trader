c72b6793d395da18964490d1ec8f3475ee227ce5
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2025-03-02 07:15:25 -0800

    updates

--- index.py
+++ index.py
@@ -24,9 +24,11 @@ import glob
 from utils.email import send_email_notification
 from utils.file_helpers import count_files_in_directory, delete_files_older_than_x_hours, is_most_recent_file_older_than_x_minutes
 from utils.price_helpers import calculate_trading_range_percentage, calculate_offset_price
+from utils.technical_indicators import calculate_market_cap_efficiency, calculate_fibonacci_levels
+# coinbase
 from utils.coinbase import get_coinbase_client, get_coinbase_order_by_order_id, place_market_buy_order, place_market_sell_order, get_asset_price, calculate_exchange_fee
 coinbase_client = get_coinbase_client()
-
+# load .env s
 load_dotenv()
 
 def load_config(file_path):
@@ -272,31 +274,7 @@ def determine_trend_2(prices, lookback_period=30, short_window=20, long_window=5
         return 'none'
 
 
-def calculate_fibonacci_levels(prices):
-    """
-    Calculate Fibonacci retracement levels for a given set of stock prices.
 
-    :param prices: deque of stock prices
-    :return: dictionary containing Fibonacci levels
-    """
-    if not prices or len(prices) < 2:
-        raise ValueError("Prices deque must contain at least two elements.")
-
-    high = max(prices)
-    low = min(prices)
-
-    # Calculate Fibonacci levels
-    diff = high - low
-    levels = {
-        'level_0': high,
-        'level_23.6': high - 0.236 * diff,
-        'level_38.2': high - 0.382 * diff,
-        'level_50': high - 0.5 * diff,
-        'level_61.8': high - 0.618 * diff,
-        'level_100': low
-    }
-
-    return levels
 
 #
 #
@@ -1046,39 +1024,7 @@ def calculate_price_changes_for_assets(directory, symbol):
     return price_change_percentage
 
 
-### Explanation:
-# Market Cap Efficiency Ratio:
-#   This ratio is calculated by dividing the current market cap by the fully diluted market cap. It gives an idea of how much of the potential market cap is currently realized.
-# Usage: A ratio closer to 1 indicates that the current market cap is close to the fully diluted market cap, suggesting that most of the potential supply is already in circulation.
-#   A lower ratio indicates that there is still a significant amount of potential supply that is not yet realized in the market cap.
-def calculate_market_cap_efficiency(market_cap, fully_diluted_market_cap):
-    """
-    Calculate the Market Cap Efficiency Ratio and provide a descriptive assessment.
 
-    :param market_cap: Current market capitalization
-    :param fully_diluted_market_cap: Fully diluted market capitalization
-    :return: Tuple containing the Market Cap Efficiency Ratio and a descriptive string
-    """
-    if fully_diluted_market_cap == 0:
-        raise ValueError("Fully diluted market cap cannot be zero.")
-
-    efficiency_ratio = market_cap / fully_diluted_market_cap
-
-    if efficiency_ratio >= 0.8:
-        description = "Almost full"
-    elif 0.5 <= efficiency_ratio < 0.8:
-        description = "Moderate"
-    else:
-        description = "Low"
-
-    return efficiency_ratio, description
-
-    # Example usage
-    # market_cap = 1949346935.5773864
-    # fully_diluted_market_cap = 2200864628.78
-    #
-    # efficiency_ratio, description = calculate_market_cap_efficiency(market_cap, fully_diluted_market_cap)
-    # print(f"Market Cap Efficiency Ratio: {efficiency_ratio:.4f} - {description}")
 
 
 #
@@ -1140,7 +1086,7 @@ def iterate_assets(interval_minutes, interval_seconds, data_points_for_x_minutes
         #
 
         coinbase_price_history_files = 'coinbase-data'
-        if is_most_recent_file_older_than_x_minutes(coinbase_price_history_files, minutes=5):
+        if is_most_recent_file_older_than_x_minutes(coinbase_price_history_files, minutes=2):
             save_new_coinbase_data(current_listed_coins_dicts, coinbase_price_history_files)
         delete_files_older_than_x_hours(coinbase_price_history_files, hours=2)
 
