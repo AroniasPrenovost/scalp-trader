0066495db291e2ee8b08681de643bfd7b5610116
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2025-04-13 16:03:47 -0700

    minor change

--- index.py
+++ index.py
@@ -31,7 +31,7 @@ from utils.coinbase import get_coinbase_client, get_coinbase_order_by_order_id, 
 coinbase_client = get_coinbase_client()
 # custom coinbase listings check
 from utils.new_coinbase_listings import check_for_new_coinbase_listings
-# load .env s
+# load .env
 load_dotenv()
 
 def load_config(file_path):
@@ -393,85 +393,6 @@ def fetch_coinmarketcap_volume_data(symbol):
         print(f"Error fetching latest data for {symbol}: {e}")
         return None
 
-
-#
-#
-# Save order data to local json ledger
-#
-
-# def save_order_data_to_local_json_ledger(symbol, order_data):
-#     """
-#     Save order data to a local file specific to the symbol.
-#     """
-#     file_name = f"{symbol}_orders.json"
-#     try:
-#         # Load existing data if the file exists and is not empty
-#         if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
-#             with open(file_name, 'r') as file:
-#                 existing_data = json.load(file)
-#         else:
-#             existing_data = []
-#
-#         # Append the new order data
-#         existing_data.append(order_data)
-#
-#         # Save the updated data back to the file
-#         with open(file_name, 'w') as file:
-#             json.dump(existing_data, file, indent=4)
-#
-#         print(f"Order data saved to {file_name}.")
-#     except json.JSONDecodeError:
-#         print(f"Error decoding JSON from {file_name}. The file might be corrupted.")
-#         # Attempt to overwrite the file with the new order data
-#         with open(file_name, 'w') as file:
-#             json.dump([order_data], file, indent=4)
-#         print(f"Order data saved to {file_name} after resetting the file.")
-#     except Exception as e:
-#         print(f"Error saving order data for {symbol}: {e}")
-
-#
-#
-# get_last_order_from_local_json_ledger
-#
-
-# def get_last_order_from_local_json_ledger(symbol):
-#     """
-#     Retrieve the most recent order from the local JSON ledger for the given symbol.
-#     """
-#     file_name = f"{symbol}_orders.json"
-#     try:
-#         if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
-#             with open(file_name, 'r') as file:
-#                 orders = json.load(file)
-#                 if orders:
-#                     # Return the most recent order
-#                     return orders[-1]
-#         print(f"No orders found in ledger for {symbol}.")
-#         return None
-#     except json.JSONDecodeError:
-#         print(f"Error decoding JSON from {file_name}. The file might be corrupted.")
-#         return None
-#     except Exception as e:
-#         print(f"Error retrieving order from ledger for {symbol}: {e}")
-#         return None
-
-#
-#
-#
-#
-
-# def reset_json_ledger_file(symbol):
-#     # Construct the file name based on the symbol
-#     file_name = f"{symbol}_orders.json"
-#
-#     # Write an empty list to the file to reset it
-#     with open(file_name, 'w') as file:
-#         json.dump([], file)
-
-#
-#
-#
-#
 
 
 
