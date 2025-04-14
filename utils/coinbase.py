626d0a85aa6346d94de201b7cbc9173a7e3dc69f
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2025-04-13 18:02:05 -0700

    adding plotting back in, need to check if we have at least 4 hours of data

--- utils/coinbase.py
+++ utils/coinbase.py
@@ -94,8 +94,6 @@ coinbase_api_secret = os.environ.get('COINBASE_API_SECRET')
 
 coinbase_spot_maker_fee = float(os.environ.get('COINBASE_SPOT_MAKER_FEE'))
 coinbase_spot_taker_fee = float(os.environ.get('COINBASE_SPOT_TAKER_FEE'))
-coinbase_stable_pair_spot_maker_fee = float(os.environ.get('COINBASE_STABLE_PAIR_SPOT_MAKER_FEE'))
-coinbase_stable_pair_spot_taker_fee = float(os.environ.get('COINBASE_STABLE_PAIR_SPOT_TAKER_FEE'))
 federal_tax_rate = float(os.environ.get('FEDERAL_TAX_RATE'))
 
 def get_coinbase_client():
