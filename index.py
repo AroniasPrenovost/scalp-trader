eb889deb4f3be9df704b3f83a16e2614de22fcf1
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2025-10-15 12:08:52 -0700

    Updates

--- index.py
+++ index.py
@@ -57,7 +57,7 @@ def load_config(file_path):
 # Define time intervals
 #
 
-INTERVAL_SECONDS = 3600 # 1 hour # 300 # 5 minute # 900 # 15 minutes
+INTERVAL_SECONDS = 900 # 15 minutes
 INTERVAL_SAVE_DATA_EVERY_X_MINUTES = (INTERVAL_SECONDS / 60)
 DATA_RETENTION_HOURS = 168 # 7 days # rolling window for analysis and storage
 
